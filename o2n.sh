dff7caf9f153f1046fd7d3fca98f747a86e98e87
MiguelDaehn <migueldaehnribeiro@gmail.com>
2025-10-07 14:57:03 -0300

    Enviando os arquivos atualizados

---
+++ o2n.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 1. Activate the Python virtual environment
+source /home/kanamori/PycharmProjects/myenv/bin/activate
+
+# 2. Run your program
+ork2notebook --filepath './OpenRocket_Quark ll (2025 - 09 - 25).ork' --ork_jar '/home/kanamori/OpenRocket/orks/OpenRocket.jar'
+
+# 3. (Optional) You can add 'deactivate' here if you want to exit the virtual environment
+# deactivate
--- ork_to_notebook.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ork2notebook --filepath '/home/kanamori/OpenRocket/orks/OpenRocket_Quark ll (2025 - 09 - 25).ork' --ork_jar '/home/kanamori/OpenRocket/orks/OpenRocket.jar'ork2notebook --filepath '/home/kanamori/OpenRocket/orks/OpenRocket_Quark ll (2025 - 09 - 25).ork' --ork_jar '/home/kanamori/OpenRocket/orks/OpenRocket.jar'
