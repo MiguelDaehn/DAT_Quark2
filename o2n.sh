e48ec49d5c49c70a39288ad3c69f758ec7a06e88
MiguelDaehn <migueldaehnribeiro@gmail.com>
2025-10-07 15:10:59 -0300

    Enviando os arquivos atualizados

--- o2n.sh
+++ o2n.sh
@@ -3,7 +3,7 @@
 source /home/kanamori/PycharmProjects/myenv/bin/activate
 
 # 2. Run your program
-ork2notebook --filepath './OpenRocket_Quark ll (2025 - 09 - 25).ork' --ork_jar '/home/kanamori/OpenRocket/orks/OpenRocket.jar'
+ork2notebook --filepath './OpenRocket_Quark ll (2025 - 09 - 25).ork' --ork_jar '../other_files/OpenRocket.jar'
 
 # 3. (Optional) You can add 'deactivate' here if you want to exit the virtual environment
 # deactivate
